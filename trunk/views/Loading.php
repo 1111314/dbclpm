ab580d3be42bf40f1f17108da77285b365365bfd
lcthanh04@student.ctu.edu.vn <lcthanh04@student.ctu.edu.vn@7d40ab71-b414-817e-e142-fd630edcea65>
2011-11-10 14:00:57 +0000

    Sửa Chitietnhatro.php,  ChitietnhatroController.php, Loading.php

--- trunk/views/Loading.php
+++ trunk/views/Loading.php
@@ -6,10 +6,22 @@
  * Time: 1:04 PM
  * To change this template use File | Settings | File Templates.
  */
-echo 'Bạn đã đăng nhập thành công!<br />';
-echo '<p style="color: blue; font-size: 10pt; font-family: tahoma">Chào mừng bạn trở lại website</p>';
-echo '<img src="../images/loading11.gif">';
 ?>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
 <meta http-equiv="refresh" content=" 5; url=../index.php" />
+<form  style="text-align:center">
+<div style=" padding-top: 60px">
+    <table align="center" style="width: 350px; border: 2px solid #9A95FD; text-align: center; padding: 10px 20px">
+        <tr>
+            <td>
+                <?php
+                    echo 'Bạn đã đăng nhập thành công!<br />';
+                    echo '<p style="color: blue; font-size: 10pt; font-family: tahoma">Chào mừng bạn trở lại website</p>';
+                    echo '<img src="../images/loading11.gif">';
+                ?>
+            </td>
+        </tr>
+    </table>
+</div>
+</form>
+
