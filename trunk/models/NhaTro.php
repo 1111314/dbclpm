b8762e6ef9b01108cd5594d0b7698863e87ecc52
lcthanh04@student.ctu.edu.vn <lcthanh04@student.ctu.edu.vn@7d40ab71-b414-817e-e142-fd630edcea65>
2011-11-10 08:38:10 +0000

    Sửa models/NhaTro.php

--- trunk/models/NhaTro.php
+++ trunk/models/NhaTro.php
@@ -7,18 +7,20 @@
 		private $mota;
 		private $sophong;
         private $qh_maso;
+        private $hinhanh;
+        private  $luotxem;
         // tong so phong
         
         /*public function __construct($maSo, $matKhau){
             $this->maSo = $maSo;
             $this->matKhau = $matKhau;
         } */
-        /*
+
         public function setMaSo($MaSo){
             $this->maso = $MaSo;
         }
         
-        */
+
         public function getMaSo(){
             return $this->maso;
         }
@@ -57,25 +59,56 @@
          public function getSoPhong(){
             return $this->sophong;
         }
-        
+        public function setHinhanh($hinhanh)
+          {
+              $this->hinhanh = $hinhanh;
+          }
+
+          public function getHinhanh()
+          {
+              return $this->hinhanh;
+          }
+
+          public function setLuotxem($luotxem)
+          {
+              $this->luotxem = $luotxem;
+          }
+
+          public function getLuotxem()
+          {
+              return $this->luotxem;
+          }
+
         
         public function themNhaTro(){
-            $this->setQuery("insert into nha_tro(QH_MA_SO,NT_TEN,NT_DIA_CHI,NT_MO_TA,NT_SO_PHONG) values ('".$this->getQHMaSo()."','".$this->getTen()."','".$this->getDiaChi()."','".$this->getMoTa()."','".$this->getSoPhong()."')");
-            return $this->executeQuery();
-        }
-        public function suaNhaTro(){
-            $this->setQuery("update nha_tro set QH_MA_SO='".$this->getQHMaSo()."',NT_TEN='".$this->getTen()."',NT_DIA_CHI='".$this->getDiaChi()."',NT_MO_TA='".$this->getMoTa()."',NT_SO_PHONG='".$this->getSoPhong()."' where NT_MA_SO='".$this->getMaSo()."'");
-			// echo $this->getQuery();
-            return $this->executeQuery();
-        }
-         public function xoaNhaTro(){
-            $this->setQuery("delete nha_tro where NT_MA_SO='".$this->getMaSo()."'");
+            $this->setQuery("insert into nha_tro(QH_MA_SO,NT_TEN,NT_DIA_CHI,NT_MO_TA,NT_SO_PHONG,NT_LUOT_XEM,NT_HINH_ANH) values
+            ('".$this->getQHMaSo()."',N'".$this->getTen()."',N'".$this->getDiaChi()."',N'".$this->getMoTa()."','".$this->getSoPhong()."','".$this->getLuotxem()."','".$this->getHinhanh()."')");
             return $this->executeQuery();
         }
 
-        
+        //Ham sửa nhà trọ: lượt xem là tự động tăng nên t viết riêng hàm, lượt xem ko có trong hàm sửa này, còn hình ảnh có cho vào thêm ko
+        // thì khi ông làm thấy cần thì cho vào, t sợ khi thêm vào câu truy vấn này nếu người ta ko sửa hình ảnh thì biến hình ảnh rỗng sợ nó báo lỗi
+        // có thể viết hàm sửa hình ảnh riêng cũng dc.
+        public function suaNhaTro(){
+            $this->setQuery("update nha_tro set QH_MA_SO='".$this->getQHMaSo()."',NT_TEN='".$this->getTen()."',NT_DIA_CHI='".$this->getDiaChi().
+                            "',NT_MO_TA='".$this->getMoTa()."',NT_SO_PHONG='".$this->getSoPhong()."' where NT_MA_SO='".$this->getMaSo()."'");
+			// echo $this->getQuery();
+            return $this->executeQuery();
+        }
+
+        public function capnhatLuotXem()
+        {
+            $this->setQuery("update nha_tro set NT_LUOT_XEM ='".$this->getLuotxem()."' where NT_MA_SO='".$this->getMaSo()."'");
+            return $this->executeQuery();
+        }
+
+         public function xoaNhaTro(){
+            $this->setQuery("delete from nha_tro where NT_MA_SO='".$this->getMaSo()."'");
+            return $this->executeQuery();
+        }
+
         public function thongtinNhaTro(){
-            $query ="select NT.NT_MA_SO,NT.NT_TEN, NT.NT_DIA_CHI,NT.NT_MO_TA,NT.NT_SO_PHONG,QH.QH_TEN,TT.TT_TEN";
+            $query ="select NT.NT_MA_SO,NT.NT_TEN, NT.NT_DIA_CHI,NT.NT_MO_TA,NT.NT_SO_PHONG,NT.NT_LUOT_XEM,NT.NT_HINH_ANH,QH.QH_TEN,TT.TT_TEN";
             $query.=" from nha_tro as NT, tinh_thanh as TT, quan_huyen as QH " ;
             $query.= " where NT.QH_MA_SO = QH.QH_MA_SO and QH.TT_MA_SO = TT.TT_MA_SO ";
             $query .= " and  NT.NT_MA_SO='".$this->getMaSo()."'";
@@ -90,11 +123,14 @@
         
         public function dsLoaiPhongThuocNhaTro(){
           $query = "select NT.NT_TEN, NT.NT_MA_SO,CLP.CLP_DON_GIA,CLP.CLP_MO_TA,LP.LP_TEN ";
-            $quey.= " from nha_tro as NT, co_loai_phong as CLP ";
+            $query.= " from nha_tro as NT, co_loai_phong as CLP ";
             $query.=" where NT.NT_MA_SO = CLP.NT_MA_SO and CLP.LP_MA_SO = LP.LP_MA_SO" ;
             $query .= " and  NT.NT_MA_SO='".$this->getMaSo()."'";
             $this->setQuery($query);
             return $this->fetchAll();
         }
-}
+
+
+
+    }
 ?>
