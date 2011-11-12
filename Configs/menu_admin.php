dd5cf6449268bc2b9f0371aa345752665f6a9be8
chgiang45 <chgiang45@student.ctu.edu.vn>
2011-11-12 02:52:40 +0000

    menu

---
+++ Configs/menu_admin.php
@@ -0,0 +1,8 @@
+<table width="800" border="0">
+  <tr>
+    <td><a href="admin.php?option=tinhthanh">Quản lý tỉnh thành</a></td>
+    <td><a href="admin.php?option=quanhuyen">Quản lý quận huyện</a> </td>
+    <td><a href="admin.php?option=quyen">Quản lý quyền</a></td>
+    <td><a href="admin.php?option=thanhvien">Quản lý thành viên</a></td>
+   </tr>
+</table>
